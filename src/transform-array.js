0f411d528fd6e385dc4ef2f2520b5ac95af2da79
divakotova <divakova.dasha@yandx.by>
2021-10-01 23:36:56 +0300

    feat: implement transfom-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+//import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,7 +13,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function transform(arr) {
+  
+ if (Array.isArray(arr) == false) {throw new Error ("'arr' parameter must be an instance of the Array!")}
+let myArr = arr.slice(0);
+   for (let i=0; i<myArr.length; i++) {
+    if (myArr[i] == '--double-next') {
+      myArr[i+1] !== undefined ? myArr.splice(i, 1, myArr[i+1]) : myArr.splice(i, 1);
+      }     
+      if (myArr[i] == '--discard-next') {
+      myArr[i+1] == undefined ? myArr.splice(i, 1) : (myArr[i+2] == '--discard-prev' || myArr[i+2] == '--double-prev') ? myArr.splice((i+1), 1) : myArr.splice(i, 2);
+     }
+      if (myArr[i] == '--double-prev') {
+        myArr[i-1] == undefined ?  myArr.splice(i, 1) : myArr[i-1] == '--discard-next' ? myArr.splice((i-1), 2) : myArr.splice(i, 1, myArr[i-1]);
+        }
+      if (myArr[i] == '--discard-prev') {
+      myArr[i-1] == undefined ? myArr.splice(i, 1) : myArr.splice((i-1), 2);
+     }
+   }
+  
+  return myArr
+ }
+ 
+
+console.log(transform([ 1, 2, 3, '--discard-next', 4, 5 ]))
+
